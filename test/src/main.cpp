91146f097668a14f3f2373faac1a7ec6b58eb2de
aleksey slovesnov <slovesnov@yandex.ru>
2021-10-30 01:29:56 +0300

    toString supports vegative numbers

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,46 +1,149 @@
 #include "aslov.h"
 
-#define ASLOV_PRINT_VARIABLE(a) printan(formatz(#a," = ",a))
-#define ASLOV_PRINT_VARIABLE_I(a) printai(formatz(#a," = ",a))
-#define ASLOV_CONCATENATE(a, b)   a##b
-#define ASLOV_FOR_EACH_NARG(...) ASLOV_FOR_EACH_NARG_(__VA_ARGS__, ASLOV_FOR_EACH_RSEQ_N())
-#define ASLOV_FOR_EACH_NARG_(...) ASLOV_FOR_EACH_ARG_N(__VA_ARGS__)
-#define ASLOV_FOR_EACH_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N
-#define ASLOV_FOR_EACH_RSEQ_N() 8, 7, 6, 5, 4, 3, 2, 1, 0
-#define ASLOV_FOR_EACH_1(f,f1,x, ...) f1(x)
-#define ASLOV_FOR_EACH_2(f,f1,x, ...) f(x)ASLOV_FOR_EACH_1(f,f1,__VA_ARGS__)
-#define ASLOV_FOR_EACH_3(f,f1,x, ...) f(x)ASLOV_FOR_EACH_2(f,f1,__VA_ARGS__)
-#define ASLOV_FOR_EACH_4(f,f1,x, ...) f(x)ASLOV_FOR_EACH_3(f,f1,__VA_ARGS__)
-#define ASLOV_FOR_EACH_5(f,f1,x, ...) f(x)ASLOV_FOR_EACH_4(f,f1,__VA_ARGS__)
-#define ASLOV_FOR_EACH_6(f,f1,x, ...) f(x)ASLOV_FOR_EACH_5(f,f1,__VA_ARGS__)
-#define ASLOV_FOR_EACH_7(f,f1,x, ...) f(x)ASLOV_FOR_EACH_6(f,f1,__VA_ARGS__)
-#define ASLOV_FOR_EACH_8(f,f1,x, ...) f(x)ASLOV_FOR_EACH_7(f,f1,__VA_ARGS__)
-#define ASLOV_FOR_EACH(N, f,f1,x, ...) ASLOV_CONCATENATE(ASLOV_FOR_EACH_,N)(f,f1,x, __VA_ARGS__)
-#define printv(...) ASLOV_FOR_EACH(ASLOV_FOR_EACH_NARG(__VA_ARGS__), ASLOV_PRINT_VARIABLE, ASLOV_PRINT_VARIABLE, __VA_ARGS__)
-#define printvi(...) ASLOV_FOR_EACH(ASLOV_FOR_EACH_NARG(__VA_ARGS__), ASLOV_PRINT_VARIABLE, ASLOV_PRINT_VARIABLE_I, __VA_ARGS__)
+#include <sys/stat.h>
+#include <map>
+#include <filesystem>
+using namespace std::filesystem;
 
-struct D{
-	int a;
-	std::string b;
-	D(){
-		a=4;
-		b="df";
-	}
-};
-std::ostream& operator <<(std::ostream& o, const D& d) {
-	return o << d.a<<" "<<d.b;
+#include <iostream>
+
+std::string ms(time_t m){
+	char b[80];
+	strftime(b, 80, "%d-%b-%Y %H:%M:%S", std::localtime(&m));
+	return b;
 }
 
+#define SORT 1
+
+struct S{
+	std::string p;
+	time_t mtime;//old
+	std::string content;
+	time_t mtime1;//new
+	std::string content1;
+
+
+	bool operator<(S& s){
+#if SORT==1
+		return mtime1<s.mtime1;
+#else
+		return mtime<s.mtime;
+#endif
+	}
+
+
+	std::string string(int i){
+		return format("%*s\t",i,p.c_str())+ms(mtime)+"\t"+ms(mtime1);
+	}
+};
+using VS=std::vector<S>;
+
+#define toString1 toString
+
 int main(int argc, char *argv[]) {
-	int i = 3;
-	std::string s="abc";
-	char c='#';
+	printl(toString1(123456,' ',3));
+	printl(toString1(12345,' ',3));
+	printl(toString1(1234,' ',3));
+	printl(toString1(123,' ',3));
+	printl(toString1(12,' ',3));
+//	printl(toString1(123456,' ',1));
+//	printl(toString1(123456));
+}
 
-	printv(i,c,s)
-	printvi(s)
+int main_(int argc, char *argv[]) {
 
-	D d;
-	printv(d.a,d.b)
-	printvi(d);
+	std::string root[]={"c:/slovesno/bridgeBad","c:/Users/user/git/bridge/bridge/src"};
+	std::string s,q,c;
+	VS v;
+	int i,j;
+
+//	auto s="c:/Users/user/git/"+name+"/"+name+"/src";
+
+	i=0;
+	for(auto r:root){
+		for (auto& p : recursive_directory_iterator(r)) {
+			if (is_directory(p)) {
+				continue;
+			}
+			auto s=p.path().string();
+			c=fileGetContent(s);
+			assert(s[r.length()]=='\\');
+
+			struct stat result;
+			stat(s.c_str(), &result);
+			auto mod_time = result.st_mtime;
+
+			auto sh=s.substr(r.length()+1);
+			if(i==0){
+				v.push_back( { sh, mod_time, c });
+			}
+			else{
+				auto it = std::find_if(v.begin(), v.end(), [&sh](const S &x) {
+					return x.p == sh;
+				});
+
+				//it==v.end()
+				//not found helper\PreferansScore.cpp 10/Oct/2021 01:04:58
+				//not found helper\PreferansScore.h 10/Oct/2021 15:51:05
+				if(it!=v.end()){
+					it->mtime1=mod_time;
+					it->content1=c;
+
+				}
+
+			}
+		}
+		i++;
+	}
+
+
+	std::sort(v.begin(),v.end());
+
+	i=0;
+	for(auto a:v){
+		i=std::max(i,int(a.p.length()));
+	}
+
+	//12-Oct-2021 19:01:28 73 1634054488
+	time_t AT=1634054488;
+	j=1;
+	std::map<time_t,int> map;
+	for(auto a:v){
+		if(a.content!=a.content1 || a.mtime1!=AT){
+			continue;
+		}
+		auto it=map.find(a.mtime1);
+		if(it==map.end()){
+			map[a.mtime1]=1;
+		}
+		else{
+			it->second++;
+		}
+		s=format("%3d",j);
+		printan(s,a.content==a.content1,a.string(i));
+		j++;
+	}
+
+	for (auto& [time, n]: map) {
+		printan(ms(time),n,time);
+	}
+
+
+/*
+	std::string s="c:\\Users\\user\\git\\stopwatch\\stopwatch\\Release\\stopwatch.exe";
+
+	struct stat result;
+	stat(s.c_str(), &result);
+	auto m = result.st_mtime;
+
+	char b[80];
+	strftime(b, 80, "%m/%d/%Y %H:%M:%S", std::localtime(&m));
+	printzi('[',b,']')
+
+	strftime(b, 80, "%d %b %Y %H:%M:%S", std::localtime(&m));
+	printzi('[',b,']')
+*/
+
+	return 0;
 
 }
