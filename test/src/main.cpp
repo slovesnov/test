5b0a905b07c02f14ab503169ec629dd163046a0f
aleksey slovesnov <slovesnov@yandex.ru>
2022-01-06 16:38:51 +0300

    deputates

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,80 +1,80 @@
 #include "aslov.h"
 #include <cstdio>
-#include <cmath>
+#include <set>
 #include <cstring>
 #include <iostream>
 //#include <numeric>
 
-uint64_t getBinomialCoefficient(int k,int n){
-	uint64_t r = 1;
-	int i;
-	/* for big n,k
-	 * C(n,k)=n*C(n-1,k-1)/k
-	 * C(n,k)=n*C(n-1,k-1)/k=(n/k)*(n-1/k-1)...(n-k+1/1)C(n-k,0); C(n-k,0)=1
-	 */
-	for (i = 1; i <= k; i++) {
-		r *= n - k + i;
-		r /= i;
-	}
-	return r;
-}
-
 int main() {
-	int i, a[4];
-	int M=3;
+	//http://duma.gov.ru/duma/deputies/8/
+	const std::string BEGIN="<strong>";
+	const std::string END="</span></span>";
+	const int N=8;
+	std::string s,s1,s2;
+	//std::stringstream buffer;
+	std::size_t p,p1,p2,p3;
+	int i,j;
+	std::set<std::string> names[N];
 
-	printl(toString(getBinomialCoefficient(10,20),','))
-
-/*
-	for(M=0;M<=8;M++){
+	for(j=0;j<N;j++){
+		std::ifstream f("c:\\downloads\\gd"+std::to_string(j+1)+".html");
+		assert(f.is_open());
+		std::stringstream buffer;//declare here
+		//buffer.clear();
+		buffer << f.rdbuf();
+		s=buffer.str();
+		p=0;
 		i=0;
-		a[0]=0;
-		for (a[1] = 0; a[1] <= M-a[0]; a[1]++) {
-			for (a[2] = 0; a[2] <= M-a[0]-a[1]; a[2]++) {
-				a[3] = M - a[0] - a[1] - a[2];
-				if (a[3] >= 0 && a[3] <= M) {
-					i++;
-				}
-				else{
-					j++;
-				}
-			}
+		std::set<std::string>& set=names[j];
+		while ( (p=s.find(BEGIN,p))!=std::string::npos){
+			p+=BEGIN.length();
+			p1=s.find(END,p);
+			assert(p1!=std::string::npos);
+			i++;
+			s1=s.substr(p, p1-p);
+			p2=s1.find('<');
+			assert(p2!=std::string::npos);
+			p3=s1.find_last_of('>');
+			assert(p3!=std::string::npos);
+			p3++;
+			p+=p3;
+			s2=s1.substr(0, p2)+" "+s1.substr(p3);
+//			if(set.find(s2)!=set.end()){
+//				printl(utf8ToLocale(s2));
+//			}
+			set.insert(s2);
+			//printl(utf8ToLocale(s2),"@");
 		}
-		assert(j==0);
-		printl(M,i,(M+1)*(M+2)/2);
+		printzn("gd",j+1," different",set.size()," all",i," size",toString(s.length(),','));
 	}
-*/
-	//a[2]+a[3]=n sum_i=0^n 1 = n+1
-
-	//a[1]+a[2]+a[3]=n sum_i=0^n (i+1) = n+1+sum_i=0^n i=n+1+n*(n+1)/2=(n+1)(1+n/2)=(n+1)(n+2)/2
-
-	//a[0]+a[1]+a[2]+a[3]=n sum_i=0^n (i+1)(i+2)/2=sum_i=0^n (i^2+3*i+2)/2=( n^3/3+n^2/2+n/6 + 3*n*(n+1)/2 + 2*(n+1) )/2
-	//	( M*M*M/3+M*M/2+M/6 + 3*M*(M+1)/2 + 2*(M+1) )/2
-
 
 	i=0;
-	for (a[0] = 0; a[0] <= M; a[0]++) {
-		for (a[1] = 0; a[1] <= M-a[0]; a[1]++) {
-			for (a[2] = 0; a[2] <= M-a[0]-a[1]; a[2]++) {
-				a[3] = M - a[0] - a[1] - a[2];
-				assert(a[3] >= 0 && a[3] <= M);
-				i++;
+	for(auto v:names[0]){
+		for(j=1;j<N;j++){
+			std::set<std::string>& set=names[j];
+			if(set.find(v)==set.end()){
+				break;
 			}
 		}
-
+		if(j==N){
+			printl(++i,utf8ToLocale(v));
+		}
 	}
-	/*
-	( v*v*v/3+v*v/2+v/6 + 3*v*(v+1)/2 + 2*(v+1) )/2)
-		= ( v*v*v/3+v*v/2+v/6 + 3*v*(v+1)/2 + 2*v )/2 +1
-		= v*( v*v/3+v/2+1./6 + 3*(v+1)/2 + 2 )/2 +1
-		= v*( v*v/3+2*v+11./3)/2 +1
-		= v*(v*v+11)/6 +v*v+1
-	*/
-	double v=M;
-	printl(i,( v*v*v/3+v*v/2+v/6 + 3*v*(v+1)/2 + 2*(v+1) )/2,v*(v*v+11)/6 +v*v+1);
-	printl(pow(2,16)*165/pow(2,20));
+/*
+	gd1 different462 all463 size787,055
+	gd2 different481 all482 size813,193
+	gd3 different465 all467 size809,463
+	gd4 different470 all471 size829,877
+	gd5 different481 all483 size876,668
+	gd6 different521 all523 size928,283
+	gd7 different491 all491 size864,214
+	gd8 different450 all450 size929,707
+	1 Грешневиков Анатолий Николаевич        main.cpp:60 main()
+	2 Жириновский Владимир Вольфович         main.cpp:60 main()
+	3 Зюганов Геннадий Андреевич             main.cpp:60 main()
+	4 Морозов Олег Викторович                main.cpp:60 main()
+	5 Харитонов Николай Михайлович           main.cpp:60 main()
+*/
 
-	printl(toString((1<<16)*165,',')," ",pow(2,16)*165/pow(2,20));
-
-
+	//printl(s.length(),i);
 }
